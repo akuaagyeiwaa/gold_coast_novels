441f024cea5e1636d6818d2f1d32efbdd2501c34
AKUA AGYEIWAA A MANIESON <gdenkyi-manieson2@huskers.unl.edu>
2025-05-17 05:59:46 -0500

    commit

--- scripts.js
+++ scripts.js
@@ -33,4 +33,18 @@ resetBtn.addEventListener('click', () => {
   fontSize = 16;
   readerText.style.fontSize = `${fontSize}px`;
 });
-K
+
+document.addEventListener("DOMContentLoaded", function () {
+      const buttons = document.querySelectorAll(".accordion-btn");
+      buttons.forEach(function (btn) {
+        btn.addEventListener("click", function () {
+          this.classList.toggle("active");
+          const content = this.nextElementSibling;
+          if (content.style.display === "block") {
+            content.style.display = "none";
+          } else {
+            content.style.display = "block";
+          }
+        });
+      });
+    });
